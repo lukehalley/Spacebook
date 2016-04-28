ca4328abc20b16e7bf79b9473b159235c778db62
luke123halley <luke123halley@gmail.com>
2016-04-28 12:54:36 +0100

    Story 11 - Tests Working

--- test/CommentTest.java
+++ test/CommentTest.java
@@ -30,19 +30,13 @@ public class CommentTest extends UnitTest {
 		bob = new User("bob", "jones", "bob@jones.com", "secret", 20, "irish");
 		post1 = new Post("Post Title 1", "This is the first post content");
 		post2 = new Post("Post Title 2", "This is the second post content");
-		comment1 = new Comment(bob, "This is the first comment content", new Date());
-		comment2 = new Comment(bob, "This is the second comment content", new Date());
 		bob.save();
 		post1.save();
 		post2.save();
-		comment1.save();
-		comment2.save();
 	}
 
 	@After
 	public void teardown() {
-		comment1.delete();
-		comment2.delete();
 		post1.delete();
 		post2.delete();
 		bob.delete();
@@ -51,6 +45,8 @@ public class CommentTest extends UnitTest {
 
 	@Test
 	public void testCreateComment() {
+		comment1 = new Comment(bob, "This is the first comment content", new Date());
+		
 		bob.posts.add(post1);
 		post1.comments.add(comment1);
 		bob.save();
@@ -61,10 +57,19 @@ public class CommentTest extends UnitTest {
 		assertEquals(1, comments.size());
 		Comment comment = comments.get(0);
 		assertEquals(comment.content, "This is the first comment content");
+		
+		bob.posts.remove(post1);
+		post1.comments.remove(comment1);
+		bob.save();
 	}
 
 	@Test
 	public void testCreateMultipleComments() {
+		
+		comment1 = new Comment(bob, "This is the first comment content", new Date());
+		comment2 = new Comment(bob, "This is the second comment content", new Date());
+		
+		
 		bob.posts.add(post1);
 		bob.posts.add(post2);
 		post1.comments.add(comment1);
@@ -82,30 +87,39 @@ public class CommentTest extends UnitTest {
 		Comment commentb = comments.get(1);
 		assertEquals(commentb.content, "This is the second comment content");
 		
+		bob.posts.remove(post1);
+		bob.posts.remove(post2);
+		post1.comments.remove(comment1);
+		post2.comments.remove(comment2);
+		bob.save();
+		
 	}
 
-//	@Test
-//	public void testDeleteComment() {
-//		
-//		bob.posts.add(post2);
-//		post2.comments.add(comment2);
-//		
-//		comment2.save();
-//		post2.save();
-//		bob.save();
-//
-//		User user = User.findByEmail("bob@jones.com");
-//		List<Post> posts = user.posts;
-//		List<Comment> comments = posts.get(0).comments;
-//		assertEquals(1, comments.size());
-//		
-//		Comment comment = comments.get(0);
-//		assertEquals(comment.content, "This is the second comment content");
-//
-//		user.posts.get(0).comments.remove(0);
-//		bob.save();
-//
-//		assertEquals(0, comments.size());
-//		
-//	}
+	@Test
+	public void testDeleteComment() {
+		
+		comment1 = new Comment(bob, "This is the first comment content", new Date());
+		
+		bob.posts.add(post1);
+		post1.comments.add(comment1);
+		bob.save();
+
+		User user = User.findByEmail("bob@jones.com");
+		List<Post> posts = user.posts;
+		List<Comment> comments = posts.get(0).comments;
+		assertEquals(1, comments.size());
+		
+		Comment comment = comments.get(0);
+		assertEquals(comment.content, "This is the first comment content");
+
+		user.posts.get(0).comments.remove(0);
+		bob.save();
+
+		assertEquals(0, comments.size());
+		
+		bob.posts.remove(post1);
+		post1.comments.remove(comment1);
+		bob.save();
+		
+	}
 }
